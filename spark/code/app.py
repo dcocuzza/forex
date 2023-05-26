c70b33dd54cfa30b2f28943e5156f1a653cb9212
dcocuzza <dcocuzza01@gmail.com>
2023-05-26 20:23:54 +0200

    aggiunta regressione lineare

--- spark/code/app.py
+++ spark/code/app.py
@@ -9,6 +9,25 @@ from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
 from pyspark.sql.dataframe import DataFrame
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.feature import VectorAssembler
+
+exchange_rates = []
+
+def linear_regression():
+  training_data = spark.createDataFrame([(float(i),) for i in exchange_rates], ["exchange_rate"])
+  assembler = VectorAssembler(inputCols=["exchange_rate"], outputCol="features")
+  prepared_data = assembler.transform(training_data)
+
+  lr = LinearRegression(featuresCol="features", labelCol="exchange_rate")
+  lr_model = lr.fit(prepared_data)
+
+
+  print("Coefficients: {}".format(lr_model.coefficients))
+  print("Intercept: {}".format(lr_model.intercept))
+
+  predictions = lr_model.transform(prepared_data)
+  predictions.show()
 
 def elaborate(batch_df: DataFrame, batch_id: int):
 
@@ -27,10 +46,12 @@ def elaborate(batch_df: DataFrame, batch_id: int):
     time_zone = api_response['Realtime Currency Exchange Rate']['7. Time Zone']
     bid_price = api_response['Realtime Currency Exchange Rate']['8. Bid Price']
     ask_price = api_response['Realtime Currency Exchange Rate']['9. Ask Price']
-    print(exchange_rate)
-   
+    #print(exchange_rate)
 
-    
+    exchange_rates.append(exchange_rate)
+    linear_regression()
+
+      
   
 sc = SparkContext(appName="PythonStructuredStreamsKafka")
 spark = SparkSession(sc)
@@ -38,6 +59,12 @@ sc.setLogLevel("WARN")
 
 kafkaServer="kafkaServer:9092"
 topic = "eurusd"
+
+
+
+
+
+
 
 # Streaming Query
 
